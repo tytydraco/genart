e5a9d97f2bee687bb58e7dd0a34b6e829d1ad408
Tyler Nijmeh <tylernij@gmail.com>
2024-05-13 21:55:12 -0700

    Add base files
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ canvas.py
@@ -0,0 +1,142 @@
+from PIL import Image, ImageColor
+import os
+import shutil
+
+
+class Canvas:
+    frame: int
+    image: Image
+
+    width: int
+    height: int
+    frames_dir: str
+
+    def __init__(self, width: int, height: int, out_dir: str = './frames'):
+        self.frame = 0
+        self.width = width
+        self.height = height
+        self.frames_dir = out_dir
+
+        self.image = Image.new('RGB', (width, height))
+
+        if os.path.isdir(out_dir):
+            shutil.rmtree(out_dir)
+
+        os.mkdir(out_dir)
+
+    def iter(self, func, frames: int, draw: bool = True):
+        """
+        Parameters
+        ----------
+        func: function
+            Function that takes an argument `frame: int` and
+            returns three arguments:
+                x: int
+                y: int
+                color: tuple[int, int, int]
+        frames: int
+            Number of frames to run through.
+        draw: bool = True
+            Draw after every iteration.
+        """
+        for frame in range(0, frames):
+            x, y, color = func(frame)
+
+            if x is None or y is None or color is None:
+                continue
+
+            self.put(x, y, color)
+            if draw:
+                self.draw()
+
+    def iters(self, funcs, frames: int, draw: bool = True):
+        """
+        Parameters
+        ----------
+        funcs: list[functions]
+            A list of function that take an argument `frame: int` and
+            returns three arguments:
+                x: int
+                y: int
+                color: tuple[int, int, int]
+        frames: int
+            Number of frames to run through.
+        draw: bool = True
+            Draw after every iteration.
+        """
+        for frame in range(0, frames):
+            for func in funcs:
+                result = func(frame)
+
+                if result is None:
+                    continue
+
+                x, y, color = result
+
+                self.put(x, y, color)
+                if draw:
+                    self.draw()
+
+    def rotate(self, degrees: float = 90, expand: bool = False, fill_color: tuple[int, int, int] = None):
+        """
+        Parameters
+        ----------
+        degrees: float = 90
+            Degrees to rotate counter-clockwise.
+        expand: bool = False
+            Expand the image size. May change dimensions.
+        fill_color: tuple[int, int, int] = None
+            Color to fill background with.
+        """
+        self.image = self.image.rotate(
+            degrees, expand=expand, fillcolor=fill_color)
+        self.width, self.height = self.image.size
+
+    def show(self, title: str = None):
+        self.image.show(title)
+
+    def apply(self, func):
+        """
+        Parameters
+        ----------
+        func: function
+            Function that takes arguments `frame: int, x: int, y: int,
+            px: tuple[int, int, int]` and returns an argument:
+                color: tuple[int, int, int]
+        """
+
+        data = self.image.getdata()
+        new_data = list(data)
+        for i in range(0, len(data)):
+            x, y = divmod(i, self.width)
+            px = self.get(x, y)
+            result = func(self.frame, x, y, px)
+            if result is None:
+                continue
+            new_data[i] = result
+        self.image.putdata(new_data)
+
+    def clear(self):
+        for x in range(0, self.width):
+            for y in range(0, self.height):
+                self.put(x, y, (0, 0, 0))
+
+    def put(self, x: int, y: int, color: tuple[int, int, int]):
+        self.image.putpixel((x, y), color)
+
+    def get(self, x: int, y: int) -> tuple[int, int, int]:
+        return self.image.getpixel((x, y))
+
+    def draw(self):
+        output = os.path.join(self.frames_dir, f'{self.frame}.png')
+        self.image.save(output, 'png')
+        self.frame += 1
+
+    def make_gif(self, fps: int = 30):
+        frames = os.listdir(self.frames_dir)
+
+        imgs = [Image.open(os.path.join(self.frames_dir, frame))
+                for frame in frames]
+        output = os.path.join(self.frames_dir, 'out.gif')
+        self.image.save(output, 'gif', append_images=imgs,
+                        save_all=True, duration=fps)
---
+++ galaxy_circle.py
@@ -0,0 +1,26 @@
+from canvas import Canvas
+import random
+
+c = Canvas(512, 512)
+
+c.apply(lambda frame, x, y, px: (random.randint(0, 255),
+        random.randint(0, 255), random.randint(0, 255)))
+
+for _ in range(0, 24):
+    for x in range(0, c.width):
+        height = random.randint(32, 128)
+
+        for y in range(0, height):
+            c.put(x, y, (0, 0, 0))
+
+    for x in range(0, c.width):
+        height = random.randint(32, 128)
+
+        for y in range(c.height - 1, c.height - 1 - height, -1):
+            c.put(x, y, (0, 0, 0))
+
+    c.rotate(15)
+    c.draw()
+
+c.show('Example')
+c.make_gif(20)
